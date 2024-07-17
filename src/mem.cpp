71bb7d2bbc7330d60ef87af1d7368ad007cc7c68
Z1KOx <119358594+Z1KOx@users.noreply.github.com>
2024-07-17 03:14:38 +0200

    Update mem.cpp

--- src/mem.cpp
+++ src/mem.cpp
@@ -5,6 +5,8 @@
 [[nodiscard]] DWORD Memory::getProcID( const std::string& procName ) const noexcept
 {
 	DWORD procID{ 0 };
+
+	// Snapshot all currently running processes
 	const auto hSnapShot{ CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, NULL) };
 
 	if ( hSnapShot != INVALID_HANDLE_VALUE )
@@ -13,14 +15,16 @@
 		ZeroMemory( &pe, sizeof(pe) );
 		pe.dwSize = sizeof( pe );
 
+		// Iterate through each process to find the specified process by name
 		for ( bool proc = Process32First(hSnapShot, &pe); proc; proc = Process32Next(hSnapShot, &pe) )
 		{
+			// Check if the current process matches the specified process name
 			if ( strcmp(pe.szExeFile, procName.c_str()) != 0 ) {
 				continue;
 			}
 
 			procID = pe.th32ProcessID;
-			break;
+			break; // Break out of the loop once the process is found
 		}
 	}
 
@@ -31,6 +35,8 @@
 [[nodiscard]] uintptr_t __fastcall Memory::getModuleBase( const std::string& modName, const DWORD procID ) const noexcept
 {
 	uintptr_t modBaseAddr{ 0 };
+
+	// Snapshot all currently loaded modules in the process
 	const auto hSnapShot{ CreateToolhelp32Snapshot(TH32CS_SNAPMODULE | TH32CS_SNAPMODULE32, procID) };
 
 	if ( hSnapShot != INVALID_HANDLE_VALUE )
@@ -39,14 +45,16 @@
 		ZeroMemory(&me, sizeof(me));
 		me.dwSize = sizeof( me );
 
+		// Iterate through each module to find the specified module by name
 		for ( bool module = Module32First(hSnapShot, &me); module; module = Module32Next(hSnapShot, &me) )
 		{
+			// Check if the current module matches the specified module name
 			if ( strcmp(me.szModule, modName.c_str()) != 0 ) {
 				continue;
 			}
 
 			modBaseAddr = reinterpret_cast<uintptr_t>(me.modBaseAddr);
-			break;
+			break; // Break out of the loop once the module is found
 		}
 	}
 
