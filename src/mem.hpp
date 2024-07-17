1dee660161b21094d979dd6179a824aa621945c7
Z1KOx <119358594+Z1KOx@users.noreply.github.com>
2024-07-17 03:16:04 +0200

    Update mem.hpp

--- src/mem.hpp
+++ src/mem.hpp
@@ -14,21 +14,26 @@ public:
 	Memory() = default;
 	~Memory() noexcept = default;
 
-	Memory( const Memory& other )            = delete;
-	Memory( Memory&& other )                 = delete;
-	Memory& operator=( const Memory& other ) = delete;
-	Memory& operator=( Memory&& other )      = delete;
+	Memory( const Memory& other )            = delete; // Copy constructor
+	Memory( Memory&& other )                 = delete; // Move constructor
+	Memory& operator=( const Memory& other ) = delete; // Copy assignment operator
+	Memory& operator=( Memory&& other )      = delete; // Move assignment operator
 
 public:
+    // Write a value of type T to a memory address at the specified offset
     template<typename T>
     void __fastcall write( const uintptr_t offset, const T value ) const noexcept { *reinterpret_cast<T*>( (*m_address + offset) ) = value; }
 
+    // Read a value of type T from a memory address at the specified offset
     template<typename T>
     [[nodiscard]] T read( const uintptr_t offset ) const noexcept { return *reinterpret_cast<T*>( (*m_address + offset) ); }
 
 public:
-	[[nodiscard]] DWORD getProcID( const std::string& procName ) const noexcept;
-	[[nodiscard]] uintptr_t __fastcall getModuleBase ( const std::string& modName, const DWORD procID ) const noexcept;
+    // Retrieve the process ID of a process with the given name
+    [[nodiscard]] DWORD getProcID( const std::string& procName ) const noexcept;
+
+    // Retrieve the base address of a module with the given name in the specified process
+    [[nodiscard]] uintptr_t __fastcall getModuleBase ( const std::string& modName, const DWORD procID ) const noexcept;
 
 private:
 	uintptr_t* m_address{ nullptr };
